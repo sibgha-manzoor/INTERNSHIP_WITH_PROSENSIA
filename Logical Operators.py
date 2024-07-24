baa653795b5b976ab3eedcccfd384f37edf58729
sibgha-manzoor <sibghamanzoor6@gmail.com>
2024-07-24 10:23:39 +0500

    Delete Logical Operators.py

--- Logical Operators.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter first Number: "))
-num2 = int(input("Enter second Number: "))
-if num1 > 0 and num2 > 0:
- print("Both are positive")
-elif num1 > 0 or num2 > 0 :
- print("One is positive")
-else :
- print("Both are negative")
