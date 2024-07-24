679ddddbe76e9e9d33f3ca55d58a81ff0c800758
sibgha-manzoor <sibghamanzoor6@gmail.com>
2024-07-24 10:27:38 +0500

    Delete Leap Year Checker.py

--- Leap Year Checker.py
+++
@@ -1,8 +0,0 @@
-year = int(input("Enter a Year: "))
-if year % 4 == 0:
-    if year % 100 != 0 or year % 400 == 0:
-         print("Year is a leap year.")
-    else:
-        print("Year is not a leap year.")
-else:
-    print("Year is not a leap year.")
