97136882cd8670e767440d08b3ef1c011edea6c5
sibgha-manzoor <sibghamanzoor6@gmail.com>
2024-07-24 10:20:25 +0500

    Delete Comparison and Logical Operators.py

--- Comparison and Logical Operators.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a Number: "))
-if num > 0 and num % 2 == 0:
- print("Positive and even")
-elif num > 0 and num % 2 == 1:
- print("Positive but odd")    
-elif num < 0:
- print("Negative")
-else :
- print("Zero")
